69f1b0b5ad761f0939cd7f9fa4011d355989b681
B SANKEERTHAN KUMAR <sankeerthankumar2003@gmail.com.com>
2026-01-16 12:47:46 +0530

    api change

--- frontend/iot-health-ui/src/api/api.js
+++ frontend/iot-health-ui/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://iot-health-backend-5pjg.onrender.com",
+  baseURL: "https://iot-healthsystem.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
