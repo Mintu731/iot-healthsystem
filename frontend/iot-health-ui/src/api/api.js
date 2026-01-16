1e19b51fb232314e0542f3d2b191bec37d0c363a
B SANKEERTHAN KUMAR <sankeerthankumar2003@gmail.com.com>
2026-01-16 10:47:57 +0530

    upated for online server

--- frontend/iot-health-ui/src/api/api.js
+++ frontend/iot-health-ui/src/api/api.js
@@ -1,20 +1,20 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "https://iot-health-backend-5pjg.onrender.com",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-//export default api;
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://iot-health-backend-5pjg.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export default api;
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:8080",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default api;
